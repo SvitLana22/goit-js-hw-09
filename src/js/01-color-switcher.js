3ac22a367600708d34b2d4650b7376b145831fb8
SvitLana Borovets <svetaborovets@ukr.net>
2022-11-05 19:59:06 +0200

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.startBtn.addEventListener('click', onStopBtnClick);
+let intervalId;
+
+function onStartBtnClick() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,12 @@
+import flatpickr from 'flatpickr';
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const refs = {};
