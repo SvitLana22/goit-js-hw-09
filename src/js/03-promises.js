468130c4a2bf32a27b62b654e9bd4724670d54fd
SvitLana Borovets <svetaborovets@ukr.net>
2022-11-08 17:29:32 +0200

    add task three

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onBtnSubmit);
+
+function onBtnSubmit(event) {
+  event.preventDefault();
+  const amountValue = Number(formRef.amount.value);
+  let delayValue = Number(formRef.delay.value);
+  const stepValue = Number(formRef.step.value);
+  for (let index = 0; index < amountValue.length; index += 1) {
+    const position = index + 1;
+    delayValue += stepValue;
+    createPromise(position, delay)
+      .then(message => Notify.success(message))
+      .catch(error => Notify.failure(error));
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
 }
